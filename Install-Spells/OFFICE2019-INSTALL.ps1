b1fba39df64fa29982b79e1a722876df8472fd6f
pat-brennan <patbrennan7@gmail.com>
2025-08-11 12:36:16 -0400

    added office 2019 install spell

---
+++ Install-Spells/OFFICE2019-INSTALL.ps1
@@ -0,0 +1,5 @@
+#Downloads the newest 0ffice 2019 files from Microsoft
+C:\OfficeSetup\setup.exe /download 'C:\OfficeSetup\configuration_(NO_OUTLOOK).xml'
+
+#Installs Office 2019
+C:\OfficeSetup\setup.exe /configure 'C:\OfficeSetup\configuration_(NO_OUTLOOK).xml'
