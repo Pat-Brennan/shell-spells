f1fa653cea69888b6abb10e37edfb3c02fe43a83
pat-brennan <patbrennan7@gmail.com>
2026-06-02 16:27:25 -0400

    disable-bluetooth spell updated to include an installation of nuget and restart for changes to apply

--- Setting-Spells/disable-bluetooth.ps1
+++ Setting-Spells/disable-bluetooth.ps1
@@ -1,8 +1,35 @@
+$BIOSPassword = $env:BiosPassword
 
-Install-Module -Name DellBIOSProvider -Force
+if([string]::IsNullOrEmpty($BiosPassword)) {
+    Write-Host "❗ Error: No BIOS password provided. Exiting script."
+    exit 1
+}
+
+if(-not(Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue)) {
+    Write-Output "🔄 Installing NuGet Package Provider..."
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+}
+
+if(-not(Get-Module -ListAvailable -Name "DellBIOSProvider" -ErrorAction SilentlyContinue)) {
+    Write-Output "🔄 Installing DellBIOSProvider module..."
+    Install-Module -Name DellBIOSProvider -Force
+}
+
 
 Import-Module DellBIOSProvider
 
-Set-Location DellSMBIOS:\
+try {
 
-Set-Item -Path "DellSMBIOS:\Wireless\BluetoothDevice" -Value "Disabled" -Password (Get-Credential -Message "Enter the password for the BIOS settings")
+  Set-Location DellSMBIOS:\
+  Set-Item -Path "DellSMBIOS:\Wireless\BluetoothDevice" -Value "Disabled" -Password $BIOSPassword
+
+  Write-Output "✅ Bluetooth Disabled Successfully."
+
+  # Restart the computer to apply changes
+  Restart-Computer -Force
+
+} catch {
+
+  Write-Error "❌ An error occurred: Please verify the BIOS password and try again. Error: $_"
+
+}
