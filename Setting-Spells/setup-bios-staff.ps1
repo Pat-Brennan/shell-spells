1ff5863d8ecde9c804be19b83f5b0cde1127de52
pat-brennan <patbrennan7@gmail.com>
2026-06-05 13:02:16 -0400

    Staff BIOS Setup spell added

---
+++ Setting-Spells/setup-bios-staff.ps1
@@ -0,0 +1,77 @@
+
+
+$NEW_BIOSPassword = $env:BiosPassword
+$CURRENT_BIOSPassword = $env:CurrentBiosPassword
+
+if([string]::IsNullOrEmpty($NEW_BIOSPassword)) {
+    Write-Host "❗ Error: No new BIOS password provided. Exiting script."
+    exit 1
+}
+
+#Installs Package Provider
+if(-not(Get-PackageProvider -Name NuGet -ListAvailable -ErrorAction SilentlyContinue)) {
+    Write-Output "🔄 Installing NuGet Package Provider..."
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+}
+
+#Installs DellBIOSProvider Module
+if(-not(Get-Module -ListAvailable -Name "DellBIOSProvider" -ErrorAction SilentlyContinue)) {
+    Write-Output "🔄 Installing DellBIOSProvider module..."
+    Install-Module -Name DellBIOSProvider -Force
+}
+
+Import-Module DellBIOSProvider
+
+try {
+
+  Set-Location DellSMBIOS:\
+
+  Write-output "⚙️ Applying BIOS settings..."
+
+    $BIOSSettings = @{
+    "DellSMBios:\Wireless\BluetoothDevice" = "Disabled"
+    "DellSMBios:\Wireless\WirelessLan" = "Disabled"
+    "DellSMBios:\PowerManagement\DeepSleepCtrl" = "Disabled"
+    "DellSMBios:\PowerManagement\BlockSleep" = "Enabled"
+    "DellSMBios:\PowerManagement\AutoOn" = "Everyday"
+    "DellSMBios:\PowerManagement\AutoOnHr" = "07"
+    "DellSMBios:\PowerManagement\AutoOnMin" = "00"
+    "DellSMBios:\SystemConfiguration\InternalSpeaker" = "Enabled"
+    "DellSMBios:\SystemConfiguration\Microphone" = "Enabled"
+  }
+
+  foreach ($setting in $BIOSSettings.GetEnumerator()) {
+    try{
+        if ([string]::IsNullOrEmpty($CURRENT_BIOSPassword)) {
+            Set-Item -Path $setting.Key -Value $setting.Value -ErrorAction Stop
+        } else {
+            Set-Item -Path $setting.Key -Value $setting.Value -Password $CURRENT_BIOSPassword -ErrorAction Stop
+        }
+        Write-Output "✅ Set $($setting.Key) to $($setting.Value)"
+    } catch {
+        Write-Warning "⚠️ Failed to set $($setting.Key): $_"
+    }
+  }
+
+  Write-Output "🔒 Configuring BIOS Password ..."
+
+  if ([string]::IsNullOrEmpty($CURRENT_BIOSPassword)) {
+    Set-Item -path "DellSMBIOS:\Security\AdminPassword" -Value $NEW_BIOSPassword
+    Write-Output "✅ BIOS password set successfully."
+  } else {
+    Set-Item -path "DellSMBIOS:\Security\AdminPassword" -Value $NEW_BIOSPassword -Password $CURRENT_BIOSPassword -ErrorAction Stop
+    Write-Output "✅ BIOS password updated successfully from Previous BIOS Password."
+    <# Action to perform if the condition is true #>
+  }
+
+  Write-Output "🎉 All BIOS settings applied successfully. System will now reboot to apply changes.
+  "
+  # Restart the computer to apply changes
+  Start-Sleep -Seconds 5
+  Restart-Computer -Force
+
+} catch {
+
+  Write-Error "❌ An error occurred during BIOS Setup. Error: $_"
+
+}
