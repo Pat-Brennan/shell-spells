51a716dc83376ecb34cf3e252689778f33b1ef70
pat-brennan <patbrennan7@gmail.com>
2026-06-17 13:53:05 -0400

    restart-windows-update added

---
+++ Practical-Spells/restart-windows-update.ps1
@@ -0,0 +1,26 @@
+
+Write-Output "ℹ️ Initializing windows update restart..."
+
+$services = @("bits", "wuauserv", "cryptSvc", "msiserver", "UsoSvc")
+
+foreach ($service in $services) {
+    try {
+        Write-Output "ℹ️ Stopping $service service..."
+        Stop-Service -Name $service -Force -ErrorAction Stop
+        Write-Output "✅ $service service stopped successfully."
+    } catch {
+        Write-Output "‼️ An error occurred while stopping $service service: $_"
+    }
+}
+
+foreach ($service in $services) {
+    try {
+        Write-Output "🔁 Restarting $service service..."
+        Start-Service -Name $service -ErrorAction Stop
+        Write-Output "✅ $service service started successfully."
+    } catch {
+        Write-Output "‼️ An error occurred while starting $service service: $_"
+    }
+  }
+
+Write-Output "✅ Windows Update services restart completed."
